574ee08e2acbabe9ff5cbce8d1038230c76979c8
Shar5K_95 <145230570+Khemrazzz@users.noreply.github.com>
2024-12-19 11:43:35 +0400

    #UI Test

--- QuickFix.mu/QuickFixMasterPage.Master.cs
+++ QuickFix.mu/QuickFixMasterPage.Master.cs
@@ -11,7 +11,10 @@ namespace QuickFix.mu
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                
+            }
         }
     }
 }
